eb54593235a9002d2a8947174dc519462dd8847b
HyunJng <khjung1654@gmail.com>
2023-07-22 21:18:10 +0900

    first commit

---
+++ src/main/java/com/holoseogi/holoseogi/HoloseogiApplication.java
@@ -0,0 +1,13 @@
+package com.holoseogi.holoseogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoloseogiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoloseogiApplication.class, args);
+    }
+
+}
--- src/main/java/com/holoseogi/holoseogi/controller/TestController.java
+++ src/main/java/com/holoseogi/holoseogi/controller/TestController.java
@@ -1,11 +1,9 @@
 package com.holoseogi.holoseogi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1")
 public class TestController {
 
     @GetMapping("/test")
