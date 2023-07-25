f9ef6bcdf1693f4d1861485b24f40c019f3c281a
HyunJng <khjung1654@gmail.com>
2023-07-26 02:18:57 +0900

    feat(debug mode): debug mode 구현
    - 로그인이 필요한 서비스들을 테스트할 때, 매번 로그인 과정을 거치는 것은 번거로우므로 debug 모드 구현
    - Authorization 헤더에 "Bearer debug"라고 작성 시 미리 생성해놓은 admin유저가 로그인된 것으로
    처리된다.

--- src/main/java/com/holoseogi/holoseogi/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/holoseogi/holoseogi/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,16 @@
 package com.holoseogi.holoseogi.filter;
 
+import com.holoseogi.holoseogi.common.UserRole;
+import com.holoseogi.holoseogi.model.entity.User;
+import com.holoseogi.holoseogi.repository.UserRepository;
+import com.holoseogi.holoseogi.security.CustomUserDetails;
 import com.holoseogi.holoseogi.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -14,6 +21,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
@@ -21,14 +31,27 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider tokenProvider;
+    private final UserRepository userRepository; // debug 모드 설정 때 필요
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String token = parseBearerToken(request);
 
+        // debug 모드
+        if (StringUtils.hasText(token) && token.equals("debug")) {
+            User admin = userRepository.findByEmail("admin@gmail.com").get();
+            Collection<? extends GrantedAuthority> authorities =
+                    Arrays.stream(UserRole.ADMIN.getRole().split(","))
+                            .map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+
+            CustomUserDetails principal = new CustomUserDetails(admin.getId(), "", authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(principal, "", authorities));
+        }
+
         // Validation Access Token
-        if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
+        else if (StringUtils.hasText(token) && tokenProvider.validateToken(token)) {
             Authentication authentication = tokenProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
             log.debug(authentication.getName() + "의 인증정보 저장");
