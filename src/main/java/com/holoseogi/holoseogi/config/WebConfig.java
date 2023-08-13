633aaf2ca6e44174e40602195e15e5dedb86892b
HyunJng <khjung1654@gmail.com>
2023-08-13 17:06:13 +0900

    fix(CORS): CORS 다른 Origin추가
    - 프론트 서버 관련 도메인 추가

--- src/main/java/com/holoseogi/holoseogi/config/WebConfig.java
+++ src/main/java/com/holoseogi/holoseogi/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://www.holoseogi.co.kr", "http://ec2-13-238-92-189.ap-southeast-2.compute.amazonaws.com:8000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
