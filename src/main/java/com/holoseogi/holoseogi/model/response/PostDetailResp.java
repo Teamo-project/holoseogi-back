2bba0a059239160292f904a5f0973d8392080fee
ameba5 <ameba5@naver.com>
2023-08-17 19:57:54 +0900

    User delete with creatorId in PostDetailResp

--- src/main/java/com/holoseogi/holoseogi/model/response/PostDetailResp.java
+++ src/main/java/com/holoseogi/holoseogi/model/response/PostDetailResp.java
@@ -11,7 +11,7 @@ public class PostDetailResp {
     private String title;
     private String content;
     private LocalDateTime createDate;
-    private Long creatorId;
+    private String creatorId; // Change type to String
     private String category;
 
     public PostDetailResp(Post post) {
@@ -19,13 +19,13 @@ public class PostDetailResp {
         this.title = post.getTitle();
         this.content = post.getContent();
         this.createDate = post.getCreateDate();
-      //  this.creatorId = post.getCreator().getId();
 
         if (post.getCreator() != null) {
-            this.creatorId = post.getCreator().getId();
+            this.creatorId = post.getCreator().getId().toString();
         } else {
-            this.creatorId = null;  // Set creatorId to null if creator is null
+            this.creatorId = "NULL";  // Set creatorId to "NULL" if creator is null
         }
+
         this.category = post.getCategory().getLabel();
     }
 }
